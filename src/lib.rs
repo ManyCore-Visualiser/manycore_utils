dc0545de73d71f8e12b4cdd96006975ce8951df3
Giuseppe Barillari <joe2k01dev@gmail.com>
2024-03-31 18:44:02 +0200

    Implement required utilities for BTreeMap <-> Vec de/serialisation
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,47 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::BTreeMap;
+
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+/// Trait to obtain BTreeMap keys from elements that should be deserialised from a Vec.
+pub trait BTreeVector<K> {
+    fn key(&self) -> K;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Deserialises a Vec<V> into a BTreeMap<K, V>
+pub fn deserialize_btree_vector<
+    'de,
+    D: Deserializer<'de>,
+    K: Ord,
+    V: Deserialize<'de> + BTreeVector<K>,
+>(
+    deserializer: D,
+) -> Result<BTreeMap<K, V>, D::Error> {
+    let vector = Vec::<V>::deserialize(deserializer)?;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    let mut ret = BTreeMap::new();
+    for element in vector {
+        let key = element.key();
+        ret.insert(key, element);
     }
+
+    Ok(ret)
+}
+
+/// Serialises a BTreeMap into a Vector
+pub fn serialise_btreemap<S: Serializer, K, V: Serialize>(
+    map: &BTreeMap<K, V>,
+    serializer: S,
+) -> Result<S::Ok, S::Error> {
+    serializer.collect_seq(map.values())
+}
+
+/// Serialises a BTreeMap into a stable-sorted Vector
+pub fn serialise_btreemap_and_sort<S: Serializer, K, V: Serialize + Ord>(
+    map: &BTreeMap<K, V>,
+    serializer: S,
+) -> Result<S::Ok, S::Error> {
+    let mut values = map.values().collect::<Vec<&V>>();
+    values.sort();
+
+    serializer.collect_seq(values)
 }
